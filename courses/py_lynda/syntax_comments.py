b0479338de8ea3754f3435fbcfe9960a03b63844
desitha.rw <rwdesitha@gmail.com>
2021-07-09 20:35:26 +0530

    -

---
+++ courses/py_lynda/syntax_comments.py
@@ -0,0 +1,8 @@
+def main():
+    print("this is the syntax.py file.")
+    egg()
+    
+def egg():
+    print("This is the egg")
+
+if __name__ == "__main__": main()
---
+++ courses/py_lynda/test.py
@@ -0,0 +1,19 @@
+class Test:
+    a=10
+    def __init__(self):
+        Test.b=20
+
+    def m1(self):
+        Test.c=30
+
+    @classmethod
+    def m2(cls):
+        Test.d=40
+        cls.e=50
+
+    
+     m3():
+        Test.f=60
+
+
+rr
